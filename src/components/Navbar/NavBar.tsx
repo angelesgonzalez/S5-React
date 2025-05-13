114d1540de27f4bbb9cbd7be8596b4681ef3d531
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-05-13 12:42:15 +0200

    Create NavLinks and BurgerMenuButton to modularise a bit more the NavBar

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,4 +1,6 @@
 import "./NavBar.css";
+import { NavLinks } from "./NavLinks";
+import { BurgerMenuButton } from "./BurgerMenuButton";
 import { Button } from "../shared/Button/Button";
 import bookmarkLogo from "../../assets/logo-bookmark.svg";
 
@@ -8,33 +10,10 @@ export function NavBar() {
 			<nav className="header-nav">
 				<img src={bookmarkLogo}></img>
 				<div className="header-nav__desktop-container">
-					<ul className="header-nav__desktop-list">
-						<li>
-							<a href="#">FEATURES</a>
-						</li>
-
-						<li>
-							<a href="#">PRICING</a>
-						</li>
-
-						<li>
-							<a href="#">CONTACT</a>
-						</li>
-					</ul>
+					<NavLinks />
 					<Button variant="secondary">LOGIN</Button>
 				</div>
-
-				<button
-					className="header-nav__burger-btn"
-					id="burger-menu"
-					aria-label="Open navigation menu">
-					<svg xmlns="http://www.w3.org/2000/svg" width="18" height="15">
-						<path
-							fillRule="evenodd"
-							d="M0 0h18v3H0V0zm0 6h18v3H0V6zm0 6h18v3H0v-3z"
-						/>
-					</svg>
-				</button>
+				<BurgerMenuButton />
 			</nav>
 		</>
 	);
---
+++ src/components/Navbar/NavLinks.tsx
@@ -0,0 +1,15 @@
+export function NavLinks() {
+	return (
+		<ul className="header-nav__desktop-list">
+			<li>
+				<a href="#">FEATURES</a>
+			</li>
+			<li>
+				<a href="#">PRICING</a>
+			</li>
+			<li>
+				<a href="#">CONTACT</a>
+			</li>
+		</ul>
+	);
+}
